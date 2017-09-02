1945443a05f71010ab3c645929c56799ac66769b
Matheus Bratfisch <matheus@loadsmart.com>
2017-09-02 01:07:28 -0300

    Add exercise step 13 part 2

---
+++ exercisestep13part2/README.md
@@ -0,0 +1,3 @@
+Exercise Break (1 point): Apply your solution to the Frequent Words Problem if Text is the oriC of Vibrio cholerae (click here to download) and k = 10. What are the most frequent words?
+
+https://stepik.org/lesson/23217/step/13?course=Where-in-the-Genome-Does-Replication-Begin-(Part-1)&unit=6784
---
+++ exercisestep13part2/exercise.py
@@ -0,0 +1,34 @@
+def frequent_words(text, k):
+    frequent_patterns = []
+    cdict = count_dict(text, k)
+    m = max(cdict.values())
+    for i in cdict:
+        if cdict[i] == m:
+            frequent_patterns.append(text[i:i + k])
+    return list(set(frequent_patterns))
+
+# Input:  Strings Pattern and Text
+# Output: The number of times Pattern appears in Text
+def pattern_count(pattern, text):
+    count = 0 # output variable
+    text_size = len(text)
+    pattern_size = len(pattern)
+    for i in range(0, text_size-pattern_size+1):
+        chop = text[i:i+pattern_size]
+        if chop == pattern:
+            count = count + 1
+    return count
+
+
+# Input:  A string Text and an integer k
+# Output: CountDict(Text, k)
+def count_dict(text, k):
+    count = {}
+    for i in range(0, len(text) - k + 1):
+        pattern = text[i:i + k]
+        count[i] = pattern_count(pattern, text)
+    return count
+
+cholerae = 'ATCAATGATCAACGTAAGCTTCTAAGCATGATCAAGGTGCTCACACAGTTTATCCACAACCTGAGTGGATGACATCAAGATAGGTCGTTGTATCTCCTTCCTCTCGTACTCTCATGACCACGGAAAGATGATCAAGAGAGGATGATTTCTTGGCCATATCGCAATGAATACTTGTGACTTGTGCTTCCAATTGACATCTTCAGCGCCATATTGCGCTGGCCAAGGTGACGGAGCGGGATTACGAAAGCATGATCATGGCTGTTGTTCTGTTTATCTTGTTTTGACTGAGACTTGTTAGGATAGACGGTTTTTCATCACTGACTAGCCAAAGCCTTACTCTGCCTGACATCGACCGTAAATTGATAATGAATTTACATGCTTCCGCGACGATTTACCTCTTGATCATCGATCCGATTGAAGATCTTCAATTGTTAATTCTCTTGCCTCGACTCATAGCCATGATGAGCTCTTGATCATGTTTCCTTAACCCTCTATTTTTTACGGAAGAATGATCAAGCTGCTGCTCTTGATCATCGTTTC'
+
+print(frequent_words(cholerae, 10))
