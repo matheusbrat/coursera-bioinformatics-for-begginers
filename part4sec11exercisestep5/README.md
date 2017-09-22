be8c1437215418d13108407f94ce3b9f7ea5b64a
Matheus Bratfisch <matheus@loadsmart.com>
2017-09-22 19:39:09 -0300

    Add part4 sec 11 exercise step 5

---
+++ part4sec11exercisestep5/README.md
@@ -0,0 +1,4 @@
+Code Challenge (3 points): Now that you have written a function CountWithPseudocounts(Motifs), write a function ProfileWithPseudocounts(Motifs) that takes a list of strings Motifs as input and returns the profile matrix of Motifs with pseudocounts as a dictionary of lists. Then add this function to Motifs.py. Make sure to use CountWithPseudocounts(Motifs) as a subroutine!
+
+
+https://stepik.org/lesson/23067/step/5?course=undefined&unit=6803
---
+++ part4sec11exercisestep5/helpers.py
@@ -0,0 +1,20 @@
+def count_with_pseudo(motifs):
+    h = {'A': [], 'C': [], 'G': [], 'T': []}
+    size = len(motifs[0])
+    for c in range(0, size):
+        for k in h.keys():
+            h[k].append(1)
+
+    for motif in motifs:
+        for c in range(size):
+            h[motif[c]][c] = h[motif[c]][c] + 1
+
+    return h
+
+
+def profile_with_pseudo(motifs):
+    c = count_with_pseudo(motifs)
+    size = len(motifs) + 4
+    for k in c.keys():
+        c[k] = list(map(lambda v: float(v)/float(size), c[k]))
+    return c
